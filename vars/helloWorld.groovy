d26e84f9adaa7d3b7e78ea16acc6c51d9aed5089
zelfick <jorgeherran@hotmail.com>
2023-04-16 18:13:02 -0500

    Add a groovysharedlib2

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,3 @@
-//def call(Map config = [:]) {
-def call(String name, String dayOfWeek) {
-    sh "echo Hello ${name}. Today is ${dayOfWeek}."
+def call(Map config = [:]) {  //Define an empty map to pass variables
+    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
